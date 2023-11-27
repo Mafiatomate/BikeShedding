a181207371578bd055719fab35a2bffea3dddd17
Mafiatomate <66642931+Mafiatomate@users.noreply.github.com>
2023-11-27 12:38:50 +0100

    IDE setup

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <ctime>
 
-struct{
+struct BikeStruct{
 
     int ArtNr;
     std::string Prodname;
@@ -12,14 +12,45 @@ struct{
         int tm_mday;//0 to 24
         int tm_mon; //0 to 11
         int tm_year;
-    }Date;
-
-} BikeStruct;
+    };
+    tm Date;
+};
 
 
 
 int main() {
+    int DBSize = 10;
+    int DBUsed[DBSize]; //0 -> free, 1 -> used
+    int option;
+    BikeStruct BikeDB[DBSize];
+    int i = 0;
 
+    std::cout << "\nBitte wählen Sie eine der folgenen Optionen\n";
+    std::cout << "[1] Liste von Artikeln anzeigen\n"
+                 "[2] Einen Artikel der Liste hinzufügen\n"
+                 "[3]\n";
 
+    std::cin >> option;
+
+    switch (option){
+        case 1:
+        //Liste anzeigen
+            break;
+        case 2:
+        //Artikel hinzufügen
+        std::cin >> BikeDB[i].ArtNr;
+        std::cin >> BikeDB[i].Prodname;
+        std::cin >> BikeDB[i].qty;
+        std::cin >> BikeDB[i].Date.tm_mday;
+        std::cin >> BikeDB[i].Date.tm_mon;
+        std::cin >> BikeDB[i].Date.tm_year;
+
+            break;
+        case 3:
+
+            break;
+        default:
+            std::cout << "Bitte geben Sie eine gültige Option ein.";
+    }
     return 0;
 }
